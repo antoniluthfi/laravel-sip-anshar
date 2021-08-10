ea7a8c86d788546bf1b9bf8de9d307f2a4bb134f
antoniluthfi <luthfiantoni027@gmail.com>
2021-08-10 09:01:44 +0700

    update cetak laporan

--- app/Http/Controllers/CetakLaporanController.php
+++ app/Http/Controllers/CetakLaporanController.php
@@ -44,7 +44,7 @@ class CetakLaporanController extends Controller
                 'data' => $data,
             ];
         } elseif($tipeLaporan === 'pesanan-penjualan') {
-            $data = PesananPenjualan::with('barang', 'pelanggan', 'penjual', 'pengirimanPesanan', 'fakturPenjualan', 'syaratPembayaran')->where('id', $str)->first();
+            $data = PesananPenjualan::with('barang', 'pelanggan', 'penjual', 'pengirimanPesanan', 'fakturPenjualan', 'syaratPembayaran')->where('kode_pesanan', $str)->first();
             $array = ['data' => $data];
         } elseif($tipeLaporan === 'pengiriman-pesanan') {
             $data = PengirimanPesanan::with('pesananPenjualan', 'fakturPenjualan', 'user', 'ekspedisi', 'cabang')->where('id_pesanan_penjualan', $str)->first();
